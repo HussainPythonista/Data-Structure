62646b5df8949bae1d691f7071434ec6965b2ae8
Mohamed Kalanthar Hussain <jaheerkalanthar816@gmail.com>
2021-09-13 13:17:14 +0530

    worked in Basic calculator but it has bug I need To clear that

--- StackDS/basicCalculator.py
+++ StackDS/basicCalculator.py
@@ -1,30 +1,41 @@
 from stack import *
 
-polynomial = "(1+(4+5+2)-3)+(6+8)"
+polynomial = "72147483647"
+#polynomial="(1+(4+5+2)-3)+(6+8)"
+#polynomial="(1 + 1)"
+#polynomial=" 2-1 + 2"
+#polynomial="(1)"
+polynomial="-2+1"
 stack=Stack()
 miniStack=Stack()
+
 def basicCalculator(polynomial):
     s=polynomial
     global stack,miniStack
     operators=["+","-"]
     i=0
-    while i<len(s)-1:
+    perform=False
+    while i<len(s):
         #print(s[i])
-        if s[i]=="(" or s[i]==")":
-            #pass
+        if s[i]=="(" or s[i]==")" or s[i]==" ":
+            #s=s.replace(s[i],"")
             i+=1
         else:
-            if s[i] not in operators:
+            if s[i] not in operators :
+                #print(s[i])
                 stack.push(s[i])
                 i+=1
-            #else:
-            if s[i]=="-" and len(stack.bucket)==1:
-                miniStack.push(s[i])
-                i+=1
-            elif s[i]=="+" and len(stack.bucket)==1:
-                miniStack.push(s[i])
-                i+=1
+            else:
+                if s[i]=="-":
+                    miniStack.push(s[i])
+                    i+=1
+                if s[i]=="+":
+                    miniStack.push(s[i])
+                    i+=1
+                perform=True
+                #print(miniStack.bucket)
             if len(stack.bucket)>=2:
+                #print(True)
                 val2=stack.pop()
                 val1=stack.pop()
                 popedVal=miniStack.pop()
@@ -34,6 +45,17 @@ def basicCalculator(polynomial):
                 if popedVal=="-":
                     operation=int(val1)-int(val2)
                     stack.push(operation)
-basicCalculator(polynomial)
+            #print(stack.bucket)   
+    if perform==True:
+        return (stack.bucket)
+
+    if perform==False:
+        ans=""
+        for i in range(len(s)):
+            if s[i]=="(" or s[i]==")" or s[i]==" ":
+                pass
+            else:
+                ans+=s[i]
+        return ans
+print(basicCalculator(polynomial))
 #print(miniStack.bucket)
-print(stack.bucket)
